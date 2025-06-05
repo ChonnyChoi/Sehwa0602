68f7b3f4afa19e7f07594cd53939d985b9bdf6dd
ChonnyChoi <chonny1130@naver.com>
2025-06-05 15:50:39 +0900

    Create my_image.py

---
+++ pages/my_image.py
@@ -0,0 +1,3 @@
+import streamlit as st
+
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNHJnc3dxYmFzZjk1ZG5xeDA5dzR3MW5qM2Y1cWNtOWpiYmluNTloMiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/xckpvtJhGi3TpQKRrW/giphy.gif", caption="예제 이미지", use_column_width=True)
